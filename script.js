3c5f488ec1107f093cd749cf84d03869f0b7cd22
TyGriffiths <griffiths.ty@gmail.com>
2016-09-24 17:06:14 -0600

    ready for review

--- script.js
+++ script.js
@@ -1,68 +1,64 @@
-(function() {
-	$('.printable').on('click', startCal)
-	function startCal() {
-//search slice(-1) to see if it can prevent user from entering -- back to back
-	
+$(document).ready(function(){
 	var countClicks = 0;
 	
-	$('span').on('click', function() {
-
-		switch ($(this).text()) {
-		case '-':
-			if (countClicks >= 2) {
-
-				}
-				else {
-					countClicks++;
-					takeAction($(this).text());
-				}
-			break;
-		default:
-			takeAction($(this).text());
-			countClicks = 0;
-		}
-
-			function takeAction() {
-			var symbolOptions = ['+', '*', '/', '.'];
-			var formula1 = $('#answer-box').text().slice(0,-1);
-			if ($(this).hasClass('symbol') && symbolOptions.indexOf($('#answer-box').text().slice(-1)) > -1) {
-				formula1 += $(this).text();
-				$('#answer-box').html(formula1);
-			}
-			else {
-				$('#answer-box').append($(this).text());
-			}
-		}
-
-		$('#equals').on('click', function() {		
-			var userInput = $('#answer-box').text();
-			var refineInput = userInput.replace(/--/g, '+');
-			var inputNum = eval(refineInput);
-			$('#answer-box').html(inputNum);
-		})
-
-		$('#reset').on('click', function() {
-			$('#answer-box').html('');
-		})
-			
-	})
-
-	console.log(countClicks);
-
-	
-	}
-})();
-
-/*alternative to switch statement
-if ($(this).text() === '-') {
+	$('.printable, .equals').on('click', function(event) {
+		event.stopPropagation();
+		var hasSymbolClass = $(this).hasClass('symbol');
+		var hasMinusId = $(this).hasClass('minus');
+		if (hasSymbolClass || hasMinusId) {
 				if (countClicks >= 2) {
-
 				}
 				else {
 					countClicks++;
-					takeAction();
+					takeAction($(this));
 				}	
 			}			
 		else {
-			takeAction();
-		}*/
+			countClicks = 0;
+			takeAction($(this));
+		}	
+	});
+
+	function takeAction(action) {
+		var symbolOptions = ['+', '*', '/', '.'];
+		var formula1 = $('#answer-box').text().slice(0,-1);
+		var hasSymbolClass = action.hasClass('symbol');
+		var answerBoxText = $('#answer-box').text();
+		var lastChar = answerBoxText.slice(-1);
+		var symbolExists = symbolOptions.indexOf(lastChar) > -1;
+		var actionText = action.text();
+		var replaceZero = $('#answer-box').text();
+		if (actionText === '=') {
+			evaluate();
+		}
+		else if(replaceZero === '0' && !hasSymbolClass) {
+			$('#answer-box').html(actionText);
+		}
+		else if (lastChar === '/' && actionText === '0') {
+			alert('Dividing by 0 is a big NO No in math. Try again');
+		}
+		else {
+			if (hasSymbolClass && symbolExists) {
+				formula1 += action.text();
+				$('#answer-box').html(formula1);
+				countClicks = 1;
+			}
+			else {	
+				$('#answer-box').append(actionText);
+			}
+		}
+	}
+
+	function evaluate() {		
+		var userInput = $('#answer-box').text();
+		var refineInput = userInput.replace(/--/g, '+');
+		var inputNum = eval(refineInput);
+		$('#answer-box').html(inputNum);
+	}
+
+	$('#reset').on('click', function() {
+		$('#answer-box').html('0');
+	});
+});
+
+
